ae1f6b406c6cbafa88e5bd33d18c0f878e690f9b
mishrpk12 <mishrapranav002@gmail.com>
2020-06-20 21:03:32 +0530

    Added few more changes

--- src/Components/QuestionPage/questions.js
+++ src/Components/QuestionPage/questions.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const Questions = () => {
     return(
-        <div>
-            Hello there!
+        <div className="questions">
+            <h1 className="text">Questions</h1>
         </div>
     )
 }
--- src/Containers/Landing.js
+++ src/Containers/Landing.js
@@ -1,4 +1,5 @@
 import React, { useEffect,useState} from 'react';
+
 import Exams from './Exams';
 import Streams from './Streams';
 import "./Landing.css";
