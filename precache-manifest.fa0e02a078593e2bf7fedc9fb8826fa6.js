3150975d4c084bd8b8ef562d55eea29147275c05
mishrpk12 <mishrapranav002@gmail.com>
2020-06-21 19:50:03 +0530

    Updates

---
+++ precache-manifest.fa0e02a078593e2bf7fedc9fb8826fa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8212469da4b4d4c5bec13ee210f7d2",
+    "url": "/ExambazaarProject/index.html"
+  },
+  {
+    "revision": "b4aa79069b56d70728f7",
+    "url": "/ExambazaarProject/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a00055a5e1e9de8e6eb4",
+    "url": "/ExambazaarProject/static/css/main.ed8e37d2.chunk.css"
+  },
+  {
+    "revision": "b4aa79069b56d70728f7",
+    "url": "/ExambazaarProject/static/js/2.d6e74f67.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ExambazaarProject/static/js/2.d6e74f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a00055a5e1e9de8e6eb4",
+    "url": "/ExambazaarProject/static/js/main.62bc127d.chunk.js"
+  },
+  {
+    "revision": "8eea85e6100f6b1609b0",
+    "url": "/ExambazaarProject/static/js/runtime-main.d8a225ff.js"
+  }
+]);
